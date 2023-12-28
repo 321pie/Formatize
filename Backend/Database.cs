ee6add76c014383c316a09ba36b9e667ad165937
Andrew Rice <aprspie@gmail.com>
2023-12-27 18:35:11 -0800

    WIP

--- Backend/Database.cs
+++ Backend/Database.cs
@@ -1,10 +1,12 @@
 namespace formAtize.Backend
 {
     using EasyDriveFilesManager;
+    using Google.Apis.Drive.v3;
+
     public class Database
     {
         private string m_localConnection = "";
-        private string m_remoteConnection = "";
+        private DriveService m_remoteConnection;
 
         public Database(string key)
         {
@@ -13,6 +15,38 @@ namespace formAtize.Backend
 
         public bool OpenConnection()
         {
+            //TODO: Local connection initialization stuff
+
+            //Initialize remote onnection to Drive
+            var tokenResponse = new TokenResponse
+            {
+                AccessToken = "...",
+                RefreshToken = "...",
+            }
+            
+            var applicationName = "..." // Use the name of the project in Google Cloud
+            var username = "..." // Use your email
+            
+            var apiCodeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = "...",
+                    ClientSecret = "..."
+                },
+                Scopes = new[] { Scope.Drive },
+                DataStore = new FileDataStore(applicationName)
+            })
+            
+            var credential = new UserCredential(apiCodeFlow, username, tokenResponse)
+            
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            })
+            m_remoteConnection = service;
+
             return true;
         }
 
